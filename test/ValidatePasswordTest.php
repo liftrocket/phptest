256d7ae75e0b0a90c68e6c90ae9a2338605dd4e5
bijiezhu <bijizhu@yahoo.com>
2017-01-08 23:00:15 -0500

    add prs-4

---
+++ test/ValidatePasswordTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace test;
+ require_once __DIR__ .'\..\vendor\autoload.php';
+//require'C:\xampp\htdocs\TestOnly\source\ValidatePassword.php';
+class ValidatePasswordTest extends \PHPUnit_Framework_TestCase{
+
+
+public function testValidLength(){
+
+
+
+  $valPass=new ValidatePassword();
+  $this->assertFalse($valPass->validLength('1234'));
+}
+}
