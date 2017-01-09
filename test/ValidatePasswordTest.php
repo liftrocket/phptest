5d8de9ad23129d2f41679c02749864bc3a0a1478
bijiezhu <bijizhu@yahoo.com>
2017-01-09 18:02:00 -0500

    update namespace

--- test/ValidatePasswordTest.php
+++ test/ValidatePasswordTest.php
@@ -1,6 +1,6 @@
 <?php
 namespace test;
- require_once __DIR__ .'\..\vendor\autoload.php';
+ require_once'../vendor/autoload.php';
 //require'C:\xampp\htdocs\TestOnly\source\ValidatePassword.php';
 class ValidatePasswordTest extends \PHPUnit_Framework_TestCase{
 
